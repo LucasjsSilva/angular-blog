085fc1ede922063fc2dc2e000c763d62af7caaa0
LucasjsSilva <luquinhajssilva@ufpi.edu.br>
2024-08-22 23:43:19 -0300

    components criadas(menu-bar, front-desk, big-card, samall card), front-desk implementada

---
+++ src/app/components/front-desk/front-desk.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-front-desk',
+  templateUrl: './front-desk.component.html',
+  styleUrls: ['./front-desk.component.css']
+})
+export class FrontDeskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
